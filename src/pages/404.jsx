9dc483f716c3e191bf06fd1d8aae247aafdee02e
Resi Respati <resir014@gmail.com>
2018-05-14 18:25:55 +0700

    initial 404 page design

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import DefaultLayout from '../components/layout/DefaultLayout';
+import Page from '../components/layout/Page';
+import PageTitle from '../components/ui/PageTitle';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+  <DefaultLayout>
+    <Page layout="page">
+      <PageTitle title="Page Not Found" />
+      <p>
+        <code>res.status(404).send(&apos;Cannot find this page.&apos;)</code>
+      </p>
+    </Page>
+  </DefaultLayout>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
